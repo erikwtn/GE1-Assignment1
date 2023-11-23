5b62dceff805270abc8a11840da456d626cb99e2
erikwtn <99894612+erikwtn@users.noreply.github.com>
2023-11-23 18:41:38 +0000

    Added unity project removed godot

--- Project/MainScene.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-var interface : XRInterface
-
-func _ready() -> void:
-	interface = XRServer.find_interface("OpenXR")
-	if interface and interface.is_initialized():
-		print("Vr is working")
-		get_viewport().use_xr = true
