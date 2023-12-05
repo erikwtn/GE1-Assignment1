881c1e9c98d6d413df57f7760c2e90654a7da5c9
erikwtn <99894612+erikwtn@users.noreply.github.com>
2023-12-05 16:50:50 +0000

    checks on interactables

---
+++ Assets/Scripts/ButtonInteract.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInteract : MonoBehaviour
+{
+    public void ButtonDown()
+    {
+        Debug.Log("Button pressed");
+    }
+}
