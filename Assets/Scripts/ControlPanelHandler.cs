ea3c3586f7dc47c0f121a7fffba28d4601c8049d
erikwtn <99894612+erikwtn@users.noreply.github.com>
2023-12-14 21:05:20 +0000

    phase 1 of fail condition added
    - animation plays on sphere
    - when sphere hits player screen goes black
    
    TODO: add heartbeat sound effect for couple seconds before moving to next scene

--- Assets/Scripts/ControlPanelHandler.cs
+++ Assets/Scripts/ControlPanelHandler.cs
@@ -7,8 +7,21 @@ public class ControlPanelHandler : MonoBehaviour
 {
     private Slider _ySlider, _vSlider, _mSlider;
     [SerializeField] private TextMeshProUGUI yTxt, vTxt, mTxt;
-    [SerializeField] private float currentYear = 0;
     
+    [SerializeField] private float currentYear = 0;
+
+    [SerializeField] private ParticleSystem vParticle, sParticle, bParticle1, bParticle2;
+    [SerializeField] private Animator sphereAnim;
+    
+    private static readonly int Failed = Animator.StringToHash("Failed");
+
+    private void Start()
+    {
+        vParticle.Stop();
+        bParticle1.Stop();
+        bParticle2.Stop();
+        sParticle.Stop();
+    }
 
     public void SliderChanged(Slider slider)
     {
@@ -33,17 +46,30 @@ public class ControlPanelHandler : MonoBehaviour
     {
         if (CheckYear())
         {
+            bParticle1.Play();
+            bParticle2.Play();
             if (CheckVoltage())
             {
+                vParticle.Play();
                 if (CheckMass())
                 {
-                    // inputs were correct and travel is successful
-                } 
-                // else fail
-            } 
-            // else fail
+                    sParticle.Play();
+                    Debug.Log("Passed");
+                }
+                else
+                {
+                    FailCondition();  
+                }
+            }
+            else
+            {
+                FailCondition();
+            }
         }
-        // else fail
+        else
+        {
+            FailCondition();
+        }
     }
 
     private bool CheckYear()
@@ -112,9 +138,16 @@ public class ControlPanelHandler : MonoBehaviour
 
     private bool CheckMass() // check if abs(years * voltage) is within tolerance and correct
     {
-        var t = _ySlider.value * _vSlider.value;
-        var c = t / 100;
+        var t = Mathf.Abs(_ySlider.value) * Mathf.Abs(_vSlider.value);
+        var c = (t / 100) * 10;
+        
+        Debug.Log("Checked mass");
 
-        return Math.Abs(_vSlider.value - t) < c;
+        return Math.Abs(_mSlider.value - t) < c;
+    }
+
+    private void FailCondition()
+    {
+       sphereAnim.SetTrigger(Failed);
     }
 }
